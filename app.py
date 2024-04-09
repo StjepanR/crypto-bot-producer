c6d48050108e94c73d1c3e970b5cf178ad231007
Stjepan Ruklić <r.stjepan00@gmail.com>
2024-04-09 11:23:41 +0200

    Refactor Coinbase socket service

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import uvicorn
+
 from coinbase.websocket import WSClientConnectionClosedException, WSClientException
-
 from service.coinbaseSocket import CoinbaseSocket
-
 from config.config import Config
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
@@ -21,14 +20,7 @@ Instrumentator().instrument(app).expose(app)
 if __name__=="__main__":
     config = Config()
 
-    try:
-        socket = CoinbaseSocket(config.coinbase_api_key, config.coinbase_api_secret)
-        socket.ws_client.open()
-        socket.subscribe("BTC-USD")
-        socket.ws_client.run_forever_with_exception_check()
-    except WSClientConnectionClosedException as e:
-        print("Connection closed! Retry attempts exhausted.")
-    except WSClientException as e:
-        print("Error encountered!")
+    socket = CoinbaseSocket(config.coinbase_api_key, config.coinbase_api_secret)
+    socket.start()
 
     uvicorn.run(app, host=config.host, port=config.port)
--- service/coinbaseSocket.py
+++ service/coinbaseSocket.py
@@ -1,7 +1,7 @@
 import json
+import logging
 
-from coinbase.websocket import WSClient
-from config.logger import logger
+from coinbase.websocket import WSClient, WSClientConnectionClosedException, WSClientException
 
 class CoinbaseSocket:
 
@@ -10,19 +10,29 @@ class CoinbaseSocket:
         print(api_secret)
         self.ws_client = WSClient(api_key=api_key, api_secret=api_secret, on_message=self.on_message, verbose=verbose)
 
+    def start(self):
+        try:
+            self.ws_client.open()
+            self.subscribe("BTC-USD")
+            self.ws_client.run_forever_with_exception_check()
+        except WSClientConnectionClosedException as e:
+            logging.error("Connection closed! Retry attempts exhausted.", e)
+        except WSClientException as e:
+            print("Error encountered!", e)
+
     def subscribe(self, channel):
         try:
 
             self.ws_client.subscribe(product_ids=list(channel), channels=["level2"])
         except:
-            logger.info("failed to subscribe to channel: " + channel)
+            logging.info("failed to subscribe to channel: " + channel)
             raise RuntimeError("failed to subscribe to channel: " + channel)
 
     def unsubscribe(self, channel):
         try:
             self.ws_client.unsubscribe(list(channel), ["heartbeats", "ticker"])
         except:
-            logger.info("failed to unsubscribe from channel: " + channel)
+            logging.info("failed to unsubscribe from channel: " + channel)
             raise RuntimeError("failed to unsubscribe from channel: " + channel)
 
     def on_message(self, message):
@@ -30,12 +40,12 @@ class CoinbaseSocket:
             message_data = json.loads(message)
             print(message_data)
         except:
-            logger.info("failed processing message")
+            logging.info("failed processing message")
             raise RuntimeError("failed processing message")
 
     def close(self):
         try:
             self.ws_client.close()
         except:
-            logger.info("closing coinbase socket failed")
+            logging.info("closing coinbase socket failed")
             raise RuntimeError("closing coinbase socket failed")
