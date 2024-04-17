0d7da95be7055894bbae36cca3d989520517aa3f
Stjepan Ruklić <r.stjepan00@gmail.com>
2024-04-17 22:06:00 +0200

    Add handling of the redundant requests

--- app.py
+++ app.py
@@ -44,11 +44,15 @@ async def list():
 async def subscribe(channel):
     logging.info("subscribing to channel " + channel)
 
-    deployment_name = channel + "-worker"
-    deployment = kubernetesService.create_deployment_object(channel, IMAGE + ":" + VERSION, 5001, deployment_name)
-    kubernetesService.create_deployment(deployment, channel + "-worker")
+    deployment_name = channel.lower() + "-worker"
 
-    deployments[deployment_name] = deployment
+    if deployment_name not in deployments.keys():
+        deployment = kubernetesService.create_deployment_object(channel, IMAGE + ":" + VERSION, 5001, deployment_name)
+        kubernetesService.create_deployment(deployment, channel + "-worker")
+
+        deployments[deployment_name] = deployment
+    else:
+        return {"message": "already subscribed to channel: " + channel}
 
     return {"message": "subscribed to channel: " + channel}
 
@@ -57,6 +61,11 @@ async def subscribe(channel):
 async def unsubscribe(channel):
     logging.info("unsubscribing from channel: " + channel)
 
-    kubernetesService.delete_deployment(channel + "-worker")
+    deployment_name = channel.lower() + "-worker"
+
+    if deployment_name in deployments.keys():
+        kubernetesService.delete_deployment(deployment_name)
+    else:
+        return {"message": "not previously subscribed to channel: " + channel}
 
     return {"message": "unsubscribed from channel: " + channel}
