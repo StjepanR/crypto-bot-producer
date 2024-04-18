c10e251987c782e9bfdf83f2727dfe89cdbd4506
Stjepan Ruklić <r.stjepan00@gmail.com>
2024-04-18 20:18:42 +0200

    Add temporal config of kafka broker url to config

--- config/config.py
+++ config/config.py
@@ -3,6 +3,6 @@ import os
 
 class Config:
     def __init__(self):
-        self.kafka_broker_url = os.environ.get("KAFKA_BROKER_URL")
+        self.kafka_broker_url = "crypto-bot-kafka.default.svc.cluster.local:9092"
         self.coinbase_api_key = os.environ.get("COINBASE_API_KEY")
         self.coinbase_api_secret = os.environ.get("COINBASE_API_SECRET")
