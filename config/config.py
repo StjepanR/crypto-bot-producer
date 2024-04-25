5b0bd275354dcf275a8ce982c57b2b53bbfd9a9c
Stjepan Ruklić <r.stjepan00@gmail.com>
2024-04-25 18:30:12 +0200

    Test secret injection

--- config/config.py
+++ config/config.py
@@ -5,4 +5,6 @@ class Config:
     def __init__(self):
         self.kafka_broker_url = "crypto-bot-kafka.default.svc.cluster.local:9092"
         self.coinbase_api_key = os.environ.get("COINBASE_API_KEY")
+        print("COINBASE_API_KEY > " + self.coinbase_api_key)
         self.coinbase_api_secret = os.environ.get("COINBASE_API_SECRET")
+        print("COINBASE_API_SECRET > " + self.coinbase_api_secret)
